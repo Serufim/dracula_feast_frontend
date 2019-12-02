a8ca3da4e7f3efc8710b4e2eecc60cfde574fc6a
Sergey Ufimtsev <serufim97@gmail.com>
2019-12-02 17:41:06 +0300

    add base routes, markup will be added soon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {RouterModule} from '@angular/router';
+import {RulesComponent} from './rules/rules.component';
+import {FaqComponent} from './faq/faq.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RulesComponent,
+    FaqComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    RouterModule.forRoot([
+      {path: '', component: MainComponent},
+      {path: 'rules', component: RulesComponent},
+      {path: 'faq', component: FaqComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
